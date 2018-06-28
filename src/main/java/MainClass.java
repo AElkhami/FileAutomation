86868dba89aebf69ea681bb4dc7ca3af48a17cf9
Ahmed22dev <ahmedeelkhami@gmail.com>
2018-06-28 15:45:31 +0200

    Update V1.2
    -Adding cutting feature to the excel file including 500,000 record into
    batches succsessfuly.
    
    -Initial implmentation for caching the fileInputStream in order to
    handle more than 500,000 record.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,97 +1,132 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.Scanner;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class MainClass {
 
-	private static final String FILE_PATH = "C:\\Users\\ElkhamiTech\\Desktop\\BookTest.xlsx";
+	private static final String FILE_PATH = "C:\\Users\\ahmed.ehab\\Desktop\\SR.xlsx";
 	private static final String SCP = "scp ";
 	private static final String Dest = "siebel:10.198.52.200";
 
 	private static XSSFWorkbook workBook;
 	private static XSSFSheet sheet;
 	private static XSSFRow row;
+	
+//	private static Workbook workBook;
+//	private static Sheet sheet;
+//	private static Row row;
 
 	private static int sheetIndex = 0;
-	private static int batchNumber = 1;
 
 	private static FileInputStream inStream;
 
 	private static String colValue;
 	private static BufferedWriter writer;
-	
+
 	private static StringBuilder sb;
+	private static int batchSize = 25000;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		BasicConfigurator.configure();
 
-		try {
-			sb =new StringBuilder(); 
-			inStream = new FileInputStream(new File(FILE_PATH));
-			readFromExcell(inStream);
+		sb = new StringBuilder();
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		File file = new File(FILE_PATH);
+
+		inStream = new FileInputStream(file);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter max row count: ");
+
+		int fileSize = Integer.valueOf(scanner.nextLine());
+
+		readFromExcell(inStream, fileSize);
+		scanner.close();
+
+	}
+
+	private static void readFromExcell(FileInputStream xinStream, int fileSize) throws IOException {
+
+		int x = 0;
+		
+		workBook = new XSSFWorkbook(xinStream);
+		sheet = workBook.getSheetAt(sheetIndex);
+		
+//		 workBook =  StreamingReader.builder()
+//		        .rowCacheSize(1000)    // number of rows to keep in memory (defaults to 10)
+//		        .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
+//		        .open(xinStream);     // InputStream or File for XLSX file (required)
+		
+		 sheet = workBook.getSheetAt(sheetIndex);
+		
+		int numberOfBatches = fileSize / batchSize;
+		for (int i = 1; i <= numberOfBatches; i++) {
+
+			genrateAttCom(x, x + batchSize);
+			writeToTextFile(SCP, sb, Dest, i);
+			xinStream.close();
+			x = x + batchSize;
+
+//			System.out.println("Choppingr " + i + " is Running");
+
 		}
 
 	}
 
-	private static void readFromExcell(FileInputStream inStream) {
+	private static void genrateAttCom(int startRow, int maxRow) {
+		Cell cell;
 
-		try {
-			workBook = new XSSFWorkbook(inStream);
-			sheet = workBook.getSheetAt(sheetIndex);
+		sb.append(SCP);
 
-			Iterator<Row> rowIterator = sheet.iterator();
+		// get cell value
+		for (int i = startRow; i < maxRow; i++) {
+			row = sheet.getRow(i);
 
-			int rowCount = sheet.getPhysicalNumberOfRows();
+//			System.out.println(String.valueOf(row.getRowNum()));
+			// Iterator<Cell> cellIterator = row.cellIterator();
+			if (row.getRowNum() == maxRow) {
 
-			while (rowIterator.hasNext()) {
+//				System.out.println("Row Number" + i + " is Running");
 
-				row = (XSSFRow) rowIterator.next();
+				break;
+			} else {
 
-				row.setRowNum(8);
-				Iterator<Cell> cellIterator = row.cellIterator();
+				// while (cellIterator.hasNext()) {
 
-				while (cellIterator.hasNext()) {
-					Cell cell = cellIterator.next();
+				cell = row.getCell(0);
+				// string builder will be used here
+				colValue = cell.getStringCellValue() + " ";
+				sb.append(colValue);
 
-					// string builder will be used here
-					colValue = cell.getStringCellValue() + " ";
-					 
-					sb.append(colValue);
-
-				}
-
+				// }
 			}
-
-			writeToTextFile(SCP, sb, Dest);
-			inStream.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 
 	}
 
-	private static void writeToTextFile(String scp2, StringBuilder stringBuilder, String dest2) throws IOException {
+	private static void writeToTextFile(String scp2, StringBuilder stringBuilder, String dest2, int batchNumber)
+			throws IOException {
 
-		String batch = scp2 + stringBuilder + dest2;
+		stringBuilder.append(dest2);
 
-		File batchFile = new File("C:\\Users\\ElkhamiTech\\Desktop\\" + "b" + String.valueOf(batchNumber) + ".sh");
+		File batchFile = new File("C:\\Users\\ahmed.ehab\\Desktop\\" + "b" + String.valueOf(batchNumber) + ".sh");
 
 		writer = new BufferedWriter(new FileWriter(batchFile));
-		writer.write(batch);
+		writer.write(String.valueOf(stringBuilder));
 		writer.close();
+		System.out.println("Batch Number" + batchNumber + " Created");
+		sb.setLength(0);
 
 	}
 
